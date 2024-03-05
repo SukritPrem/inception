a136818c0a5ef395d168620ca238577834339b0b
Sukrit Pipitkul <spipitku@z2t5c1.42bangkok.com>
2024-03-05 21:12:19 +0700

    need fixed nginx port to connect with wordpress

--- wordpress/entrypoint.sh
+++ wordpress/entrypoint.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-
 service php7.4-fpm start
 
+sleep 5
+
+service php7.4-fpm stop
+
+wget -O /tmp/wordpress.tar.gz https://wordpress.org/latest.tar.gz 
+
+mkdir /var/www
+
+mkdir /var/www/html
+
+tar -xzvf /tmp/wordpress.tar.gz -C /var/www/html 
+
+chown -R www-data.www-data /var/www/html/wordpress 
+
+chmod -R 755 /var/www/html/wordpress
 
 # Keep the container running
 exec "$@"
