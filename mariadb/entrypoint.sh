733bef9e3fb945ada81fca555a527d3231cf1ea8
Sukrit pipitkul <Sukrit.pi@ku.th>
2024-03-08 21:28:00 +0700

    forgot push yml

--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+
+
 service mariadb start
 
 sleep 5
 
+mkdir test
+
 service mariadb stop
+
+chown -R mysql:mysql /var/lib/mysql  # Change ownership to mysql user and group
+
+chmod -R 755 /var/lib/mysql  
 
 exec "$@"
