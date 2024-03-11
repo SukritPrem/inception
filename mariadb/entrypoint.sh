8304596e15209552535bee39ce6758eab5c5c2cf
sukrit.pi@ku.th <sukrit.pi@ku.th>
2024-03-11 04:03:13 -0400

    need test

--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -6,7 +6,27 @@ service mariadb start
 
 sleep 5
 
-mkdir test
+if [ -d "/var/lib/mysql/$DB_NAME" ]; then
+    # Move database_dump.sql to /var/lib/mysql/$DB_NAME directory if $DB_NAME directory exists
+    mv database_dump.sql "/var/lib/mysql"
+    echo "Hello"
+    if [ -f "/var/lib/mysql/database_dump.sql" ]; then
+        echo "Hi"
+        mysql -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" < "/var/lib/mysql/database_dump.sql"
+    fi
+    echo "Hello1"
+else
+    mariadb <<EOF
+    CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+    CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+    GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+    FLUSH PRIVILEGES;
+EOF
+
+    # Dump the database into /var/lib/mysql/$DB_NAME/database_dump.sql
+    mysql -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > "/var/lib/mysql/database_dump.sql"
+fi
+
 
 service mariadb stop
 
--- wordpress/entrypoint.sh
+++ wordpress/entrypoint.sh
@@ -18,5 +18,14 @@ chown -R www-data.www-data /var/www/html/wordpress
 
 chmod -R 755 /var/www/html/wordpress
 
+cd /var/www/html/wordpress
+
+cp wp-config-sample.php wp-config.php
+
+# Generate wp-config.php content
+sed -i "s/database_name_here/$DB_NAME/" wp-config.php
+sed -i "s/username_here/$DB_USER/" wp-config.php
+sed -i "s/password_here/$DB_PASSWORD/" wp-config.php
+sed -i "s/localhost/$DB_HOST/" wp-config.php
 # Keep the container running
 exec "$@"
