20a9ca02fb4b3e81c6d44845a6c8f609fa6de1dc
sukrit.pi@ku.th <sukrit.pi@ku.th>
2024-03-13 02:56:43 -0400

    new dump mysql

--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -8,14 +8,15 @@ sleep 5
 
 if [ -d "/var/lib/mysql/$DB_NAME" ]; then
     # Move database_dump.sql to /var/lib/mysql/$DB_NAME directory if $DB_NAME directory exists
+    echo "Hello1"
     mv database_dump.sql "/var/lib/mysql"
-    echo "Hello"
     if [ -f "/var/lib/mysql/database_dump.sql" ]; then
         echo "Hi"
         mysql -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" < "/var/lib/mysql/database_dump.sql"
     fi
-    echo "Hello1"
+    mysqldump -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > "/var/lib/mysql/database_dump.sql"
 else
+    echo "Hello2"
     mariadb <<EOF
     CREATE DATABASE IF NOT EXISTS ${DB_NAME};
     CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
@@ -24,7 +25,7 @@ else
 EOF
 
     # Dump the database into /var/lib/mysql/$DB_NAME/database_dump.sql
-    mysql -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > "/var/lib/mysql/database_dump.sql"
+    mysqldump -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > "/var/lib/mysql/database_dump.sql"
 fi
 
 
