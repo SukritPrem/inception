276f0a3dd061df57773d6bd5a5d43de91af03e8d
sukrit.pi@ku.th <sukrit.pi@ku.th>
2024-03-17 02:37:03 -0400

    add ftp-server

--- ftp/entrypoint.sh
+++ ftp/entrypoint.sh
@@ -7,7 +7,12 @@ useradd -m myftpuser
 # Set password for the user (replace 'password' with your desired password)
 echo 'myftpuser:password' | chpasswd
 
-
 mkdir -p /var/run/vsftpd/empty
+
+mkdir -p /var/www/html
+
+chown myftpuser:myftpuser /var/www/html
+
+chmod -R 755 /var/www/html
 
 exec "$@"
